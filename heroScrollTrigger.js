05dcfeb2c67acf7d2b39065afb9db057b92db887
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-04-21 05:11:46 +0800

    Create heroScrollTrigger.js
    Starting the file for hero scroll trigger.

---
+++ heroScrollTrigger.js
@@ -0,0 +1,47 @@
+import { gsap } from "gsap"; 
+
+const pet1 = document.querySelector(".hero_pet01-wrap")
+const pet2 = document.querySelector(".hero_pet02-wrap")
+const pet3 = document.querySelector(".hero_pet03-wrap")
+const pet4 = document.querySelector(".hero_pet04-wrap")
+const content = document.querySelector(".hero_content-wrap")
+const section2 = document.querySelector(".is-second-section")
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollTrigger)
+
+  const tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: section2 ,
+      start: "top 50%",
+      end: "bottom 50%", 
+      scrub: true,
+      markers: true,
+    }
+  })
+
+  tl.to(pet1, {
+    z: 100,
+    ease: "power4.out",
+  })
+
+  tl.to(pet2, {
+    z: 100,
+    ease: "power4.out",
+  })
+
+  tl.to(content, {
+    z: 100,
+    ease: "power4.out",
+  })
+
+  tl.to(pet2, {
+    z: 100,
+    ease: "power4.out",
+  })
+
+  tl.to(content, {
+    z: 100,
+    ease: "power4.out",
+  })
+})
