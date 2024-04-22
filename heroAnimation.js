5c4e4702d911204d2a3ea73ea6c61372f35e2292
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-04-23 04:24:36 +0800

    Update heroAnimation.js
    Updating the animation to accept toggleActions.

--- heroAnimation.js
+++ heroAnimation.js
@@ -15,60 +15,62 @@ const section2 = document.querySelector(".is-second-section")
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger);
 
-  const tl = gsap.timeline();
+  const tl = gsap.timeline({
+    paused: true,
+    reversed: true,
+  );
+
+  tl.to(pet1, {
+    xPercent: 300,
+    yPercent: -300,
+    scale: 4,
+    ease: "power4.in",
+    duration: 0.5
+  })
+    
+  .to(pet2, {
+    xPercent: -300,
+    yPercent: 300,
+    scale: 4,
+    ease: "power4.in",
+    duration: 0.5
+  }, "-=0.25") // overlap for seamless transition
+    
+  .to(content, {
+    xPercent: 100,
+    scale: 100,
+    ease: "power4.in",
+    duration: 0.5
+  }, "-=0.25") // overlap for seamless transition
+    
+  .to(pet3, {
+    xPercent: 300,
+    yPercent: 300,
+    scale: 4,
+    ease: "power4.in",
+    duration: 0.5
+  }, "-=0.25") // overlap for seamless transition
+    
+  .to(pet4, {
+    xPercent: -300,
+    yPercent: -300,
+    scale: 4,
+    ease: "power4.in",
+    duration: 0.5
+  }, "-=0.25") // overlap for seamless transition
+    
+  .to(glow, {
+    opacity: 0,
+    ease: "power4.out",
+    duration: 0.5
+  }, "-=0.25"); // overlap for seamless transition
 
   ScrollTrigger.create({
+    animation: tl,
     trigger: section2,
     start: "top 90%",
     end: "bottom top",
     toggleActions: "play pause reverse none",
     markers: true,
-    onEnter: () => { // This will start the timeline when the trigger is reached
-      
-      tl.to(pet1, {
-        xPercent: 300,
-        yPercent: -300,
-        scale: 4,
-        ease: "power4.in",
-        duration: 0.5
-      })
-        
-      .to(pet2, {
-        xPercent: -300,
-        yPercent: 300,
-        scale: 4,
-        ease: "power4.in",
-        duration: 0.5
-      }, "-=0.25") // overlap for seamless transition
-        
-      .to(content, {
-        xPercent: 100,
-        scale: 100,
-        ease: "power4.in",
-        duration: 0.5
-      }, "-=0.25") // overlap for seamless transition
-        
-      .to(pet3, {
-        xPercent: 300,
-        yPercent: 300,
-        scale: 4,
-        ease: "power4.in",
-        duration: 0.5
-      }, "-=0.25") // overlap for seamless transition
-        
-      .to(pet4, {
-        xPercent: -300,
-        yPercent: -300,
-        scale: 4,
-        ease: "power4.in",
-        duration: 0.5
-      }, "-=0.25") // overlap for seamless transition
-        
-      .to(glow, {
-        opacity: 0,
-        ease: "power4.out",
-        duration: 0.5
-      }, "-=0.25"); // overlap for seamless transition
-    }
   });
 });
