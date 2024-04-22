0774f41d0f7ec9da6059450293278c47db054f77
kyonyhk <67498165+kyonyhk@users.noreply.github.com>
2024-04-23 00:13:09 +0800

    Update heroAnimation.js
    Adding all the variables.

--- heroAnimation.js
+++ heroAnimation.js
@@ -1,3 +1,17 @@
+const pet1 = document.querySelector(".hero_pet01-wrap")
+const pet2 = document.querySelector(".hero_pet02-wrap")
+const pet3 = document.querySelector(".hero_pet03-wrap")
+const pet4 = document.querySelector(".hero_pet04-wrap")
+
+const content = document.querySelector(".hero_content-container")
+const logo = document.querySelector(".hero_genopets-logo-wrap")
+const heading = document.querySelector(".hero_heading-wrap")
+const download = document.querySelector(".hero_download-wrap")
+
+const glow = document.querySelector(".hero_glow-wrap")
+
+const section2 = document.querySelector(".is-second-section")
+
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger);
 
